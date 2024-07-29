df6899ded7ef2aad97dc5d19dd4384750ee6aae4
Penut Chen <penut.chen@intelli-go.com>
2024-07-29 21:21:36 +0800

    update crawl-data.py

--- crawl-data.py
+++ crawl-data.py
@@ -1,34 +1,26 @@
 import json
-from concurrent.futures import ThreadPoolExecutor
+import os
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 import requests
+from tqdm import trange
 
 
 def main():
     version = get_version()
     champions: dict[str, dict] = get_champions(version)
+    with open("data/champion.json", "wt", encoding="UTF-8") as fp:
+        json.dump(champions, fp, ensure_ascii=False, indent=4)
 
-    champs = list(champions["data"].keys())
+    with ThreadPoolExecutor(max_workers=16) as executor:
+        futures = [
+            executor.submit(get_champ, champ, lang, version)
+            for champ in champions["data"]
+            for lang in ("en_US", "zh_TW")
+        ]
 
-    cdn_url = f"http://ddragon.leagueoflegends.com/cdn/{version}/data"
-    url_fmt = "{}/{}/champion/{}.json"
-    langs = ("en_US", "zh_TW")
-
-    args = [(lang, champ) for champ in champs for lang in langs]
-
-    def get_champ(args):
-        i, (lang, champ) = args
-        url = url_fmt.format(cdn_url, lang, champ)
-        resp = requests.get(url)
-
-        fn = f"data/{lang}/{champ}.json"
-        with open(fn, "wt", encoding="UTF-8") as fp:
-            fp.write(resp.text)
-
-        print(f"{i} Done")
-
-    with ThreadPoolExecutor(max_workers=12) as executor:
-        executor.map(get_champ, enumerate(args))
+        with trange(len(futures), ncols=100) as prog:
+            [prog.update() for _ in as_completed(futures)]
 
 
 def get_version(offset=0):
@@ -41,6 +33,17 @@ def get_champions(version):
     url = f"http://ddragon.leagueoflegends.com/cdn/{version}/data/en_US/champion.json"
     resp = requests.get(url)
     return json.loads(resp.text)
+
+
+def get_champ(champ, lang, version):
+    cdn_url = f"http://ddragon.leagueoflegends.com/cdn/{version}/data"
+    url = f"{cdn_url}/{lang}/champion/{champ}.json"
+    resp = requests.get(url)
+
+    fn = f"data/{lang}/{champ}.json"
+    os.makedirs(f"data/{lang}", exist_ok=True)
+    with open(fn, "wt", encoding="UTF-8") as fp:
+        fp.write(resp.text)
 
 
 if __name__ == "__main__":
